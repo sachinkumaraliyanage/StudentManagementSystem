022f061987a3f1dca873976dc20a30ed3d877d8d
Sachin Kumara Liyanage <sachinkumaraliyanage@gmail.com>
2024-06-12 16:29:04 +0530

    sachin: add teacher test Data

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,22 @@ class TeacherFactory extends Factory
      */
     public function definition(): array
     {
+
         return [
-            //
+            'fname' => fake()->firstName(),
+            'lname' => fake()->lastName(),
+            'nic' => fake()->unique()->randomNumber(9) . 'V',
+            'name_prefix' => fake()->randomElement(['Mr', 'Mrs', 'Miss', 'Dr', 'Prof', 'Rev', 'Other']),
+            'gender' => fake()->randomElement(['Male', 'Female', 'Other']),
+            'pno' => fake()->unique()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'address' => fake()->address(),
+            'school' => fake()->company(),
+            'description' => fake()->text(),
+            'updated_by' => User::get()->random()->id,
+            'created_by' => User::get()->random()->id,
+            'state' => fake()->randomElement([0, 1]),
+
         ];
     }
 }
